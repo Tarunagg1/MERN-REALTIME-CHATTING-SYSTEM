1ee41c9932e12f5455e8e0fe0a97d9fbc2d4d19f
--add <tarun@iotric.com>
2023-05-22 17:51:54 +0530

    auth system

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+const connectDB = require('./config/db');
 
 const app = express();
-
+connectDB();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to accept json data
 
-app.get('/', (req, res) => {
-    return res.status(200).json({ message: 'OK' });
-});
-
-app.get('/api/chat', (req, res) => {
-    return res.status(200).json({ message: 'OK' });
-});
-
-
-app.get('/api/chat/:id', (req, res) => {
-    return res.status(200).json({ message: 'OK' });
-});
-
+app.use("/api/user", userRoutes);
+// app.use("/api/chat", chatRoutes);
+// app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log('listening on port ' + PORT);
